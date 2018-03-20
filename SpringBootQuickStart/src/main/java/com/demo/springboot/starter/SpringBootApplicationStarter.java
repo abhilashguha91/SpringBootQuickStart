56eec3125562e39e7d979935f97b2a9234149f50
abguha <abguha@ABGUHA-LAP.oradev.oraclecorp.com>
2018-03-21 02:33:49 +0530

    new commit

---
+++ SpringBootQuickStart/src/main/java/com/demo/springboot/starter/SpringBootApplicationStarter.java
@@ -0,0 +1,16 @@
+package com.demo.springboot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationStarter {
+
+	
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootApplicationStarter.class, args);	
+		
+	}
+
+}
