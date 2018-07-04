19a53f97774b39abf093e2af187743192b12caf9
abguha <abguha@ABGUHA-LAP.oradev.oraclecorp.com>
2018-07-04 14:48:10 +0530

    second commit

--- SpringBootQuickStart/src/main/java/com/demo/springboot/starter/SpringBootApplicationStarter.java
+++ SpringBootQuickStart/src/main/java/com/demo/springboot/starter/SpringBootApplicationStarter.java
@@ -1,15 +1,18 @@
 package com.demo.springboot.starter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {"com.demo.springboot"})
+@EnableAutoConfiguration	
 public class SpringBootApplicationStarter {
 
-	
+	 
 	public static void main(String[] args) {
 
-		SpringApplication.run(SpringBootApplicationStarter.class, args);	
+		SpringApplication.run(SpringBootApplicationStarter.class, args);
 		
 	}
 
